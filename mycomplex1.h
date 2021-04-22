c264162a55dcda88f4fe55e8c5585255f6e09198
NastyaChevt <chevtaevd@mail.ru>
2021-04-22 12:53:27 +0500

    Добавлены комментарии mycomplex1.h

---
+++ mycomplex1.h
@@ -0,0 +1,65 @@
+/*!
+    @file
+    @brief Заголовочный файл класса Complex.
+*/
+#ifndef _MY_COMPLEX_H_
+#define _MY_COMPLEX_H_
+#include <iostream>
+
+using namespace std;
+
+/*!
+    @brief Class of a complex number
+*/
+class Complex
+{
+    /*!
+        @brief Действительная составляющая
+    */
+    double Re;
+
+    /*!
+        @brief Мнимая составляющая
+    */
+    double Im;
+
+    /*!
+        @brief List of class members.
+    */
+    public:
+        Complex(double aRe = 0, double aIm = 0);
+        Complex(const Complex &);
+        ~ Complex();
+
+        void Set(double aRe, double aIm = 0);
+        operator double();
+
+        double abs();
+
+        friend istream & operator >> (istream &, Complex &);
+
+        friend ostream & operator << (ostream &, Complex &);
+        Complex operator + (const Complex &);
+        Complex operator - (const Complex &);
+        Complex operator + (const double &);
+
+        friend Complex operator + (const double &, const Complex &);
+        Complex operator - (const double &);
+
+        friend Complex operator - (const double &, const Complex &);
+        Complex operator * (const Complex &);
+        Complex operator * (const double&);
+
+        friend Complex operator * (const double &, const Complex &);
+        Complex operator / (const double &);
+        Complex & operator += (const Complex &);
+        Complex & operator -= (const Complex &);
+        Complex & operator *= (const Complex &);
+        Complex & operator += (const double &);
+        Complex & operator -= (const double &);
+        Complex & operator *= (const double &);
+        Complex & operator /= (const double &);
+        Complex & operator = (const Complex &);
+        Complex & operator = (const double &);
+};
+#endif
